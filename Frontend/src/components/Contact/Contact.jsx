952dd903f77cf778fe861a933dd08f2774bea37e
resoluteKid <shigvansagar28@gmail.com>
2025-05-05 16:52:47 +0530

    some minor changes done

--- Frontend/src/components/Contact/Contact.jsx
+++ Frontend/src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Contact.css';
 import { useNavigate } from 'react-router-dom'; 
 
+
 function Contact() {
 
   const navigate = useNavigate();
@@ -62,7 +63,9 @@ function Contact() {
           
         </div>
 
-        <div className="right"></div>
+        <div className="right">
+          <img src="" alt="" />
+        </div>
 
       </div>
     </div>
